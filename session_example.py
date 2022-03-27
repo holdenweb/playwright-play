5c617db2419cea83404e5d6bfc54784b818d4b69
Steve Holden <steve@holdenweb.com>
2022-03-27 10:57:23 +0100

    Establish a way through the anonymous path.
    This was me, learning how to navigate the popups an anonymous user
    sees starting at trade.gov.uk and navigating to great.gov.uk.

--- session_example.py
+++ session_example.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import Playwright, sync_playwright, expect, TimeoutError as PWTimeoutError
 
 
 def run(playwright: Playwright) -> None:
@@ -9,7 +9,10 @@ def run(playwright: Playwright) -> None:
     page = context.new_page()
 
     # Go to https://www.gov.uk/government/organisations/department-for-international-trade
-    page.goto("https://www.gov.uk/government/organisations/department-for-international-trade")
+    page.goto(
+        "https://www.gov.uk/government/organisations/department-for-international-trade",
+        wait_until="domcontentloaded"
+    )
 
     # Click text=Accept additional cookies
     lctr = page.locator("text=Accept additional cookies")
@@ -19,22 +22,29 @@ def run(playwright: Playwright) -> None:
     page.locator("text=Hide this message").click()
 
     # Click [aria-label="Cookies consent manager"] >> text=Accept all cookies
-    lctr1 = page.locator("[aria-label=\"Cookies consent manager\"] >> text=Accept all cookies")
-    lctr1.click()
-
-    # Click [aria-label="Close this message"]
-    page.locator("[aria-label=\"Close this message\"]").click()
-
-    # Click text=Where to export
-    page.locator("text=Where to export").click()
-    # expect(page).to_have_url("https://www.great.gov.uk/signup/?next=/where-to-export/")
+    # Handles cookie consent management if present
+    try:
+        if page.locator(
+            "[aria-label=\"Close this message\"]"
+        ).count():
+        # Click [aria-label="Close this message"]
+            page.locator("[aria-label=\"Close this message\"]").click()
+    except PWTimeoutError:
+        pass
 
     # Go to https://www.great.gov.uk/?utm_source=govuk&utm_medium=homepagelink&utm_campaign=EIG
-    page.goto("https://www.great.gov.uk/?utm_source=govuk&utm_medium=homepagelink&utm_campaign=EIG")
+    page.goto(
+        "https://www.great.gov.uk/?utm_source=govuk&utm_medium=homepagelink&utm_campaign=EIG",
+        wait_until="domcontentloaded"
+    )
+
+    # expect(page).to_have_url("https://www.great.gov.uk/campaigns/internationalisation-fund-for-english-businesses/")
+    loc = page.locator(".button:visible:has-text('Accept all cookies')")
+    if loc.count():
+        loc.click()
 
     # Click text=Internationalisation Fund now open
     page.locator("text=Internationalisation Fund now open").click()
-    # expect(page).to_have_url("https://www.great.gov.uk/campaigns/internationalisation-fund-for-english-businesses/")
 
     # Close page
     page.close()
